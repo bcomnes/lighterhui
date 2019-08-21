ce6a3822e5b71b1715dc7a4192dfb7f4864770fe
Bret Comnes <bcomnes@gmail.com>
2019-08-21 17:42:11 +0200

    change name to ligherhui

--- wired.js
+++
@@ -1,8 +0,0 @@
-const Component = require('./')
-const { rendner } = require('lighterhtml')
-
-module.exports = class MorphComponent extends Component {
-  render () {
-    render(this.element, this.createElement)
-  }
-}
